d721dcd4ccc28e92138e86623be371143db425ca
bighappyworld <bighappyworld@gmail.com>
2014-11-24 09:27:50 +0700

    added tests

--- tests/test.js
+++ tests/test.js
@@ -5,23 +5,42 @@ describe('email-verify', function(){
   var verifier = require( '../index.js' );
 
   describe('#verify()', function(){
-    it('should respond with an object where success is true', function(done){
+    it('existing email: should respond with an object where success is true', function(done){
       verifier.verify("rob@below.io", function(info,err){
         assert(info.success);
         done();
       });
-    })
-    it('should respond with an object where success is false', function(done){
+    });
+    it('non-existing email: should respond with an object where success is false', function(done){
       verifier.verify("antirob@below.io", function(info,err){
         assert(!info.success);
         done();
       });
-    })
-    it('should respond with an object where success is false', function(done){
+    });
+    it('badly formed email: should respond with an object where success is false', function(done){
       verifier.verify("badlyformed##email@email@.com", function(info,err){
         assert(!info.success);
         done();
       });
-    })
-  })
-})
+    });
+    it('short timeout: should respond with an object where success is false', function(done){
+      verifier.verify("rob@below.io", { timeout: 1, port: 25 }, function(info,err){
+        assert(!info.success);
+        done();
+      });
+    });
+    it('long timeout: should respond with an object where success is true', function(done){
+      verifier.verify("rob@below.io", { timeout: 5000, port: 25 }, function(info,err){
+        assert(info.success);
+        done();
+      });
+    });
+    it('bad smtp port: should respond with an object where success is false', function(done){
+      verifier.verify("rob@below.io", { port: 587 }, function(info,err){
+        assert(!info.success);
+        done();
+      });
+    });
+
+  }); // End Describe #verify()
+}); // End Describe email-verify
