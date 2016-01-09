e35821eb2f0ea9907d366dfcc0201566463c227c
sancowinx <jm-sarn@hotmail.com>
2016-01-09 15:36:42 +0700

    added file extension checking. Some dev. force throw error
    currently has one forced throw error, for debugging only.

--- methods/readfromfile.js
+++ methods/readfromfile.js
@@ -2,18 +2,27 @@ var fs = require('fs');
 var _ = require('lodash');
 
 module.exports.getAddressFromTextFile = function(filepath) {
-  var isExist = true;
+  var file = { isExist: true, extension: '' };
+
+  file.isExist = true;
+  file.extension = require('path').extname(filepath);
+
+    console.log("extension: ", file.extension);
+    if (file.extension === '.txt')  console.log('It\'s text');
+    else console.log('it\'s not text');
+
+    // forced throw error, check extension
+    throw new Error('forced throw error, check extension');
+
   try {
     var fileContent = fs.readFileSync(filepath, 'utf-8');
-  } catch (e){
+  } catch (e) {
     if (e.code === 'ENOENT') console.log('File not found!');
-    else console.log('Another error: ', e);
-    isExist = false;
-  }
-  finally {
-
-    // if file is not found, do not run this!
-    if (!isExist) {
+    else console.log('Error: ', e);
+    file.isExist = false;
+  } finally {
+    if (!file.isExist) {
+      // if file is not found, do not run this!
       throw new Error("SOMETHING BROKE")
     } else {
       var addressList = fileContent.split("\n");
@@ -23,4 +32,4 @@ module.exports.getAddressFromTextFile = function(filepath) {
   }
 }
 
-// TODO: Clean up, if posible
+// TODO: Clean up, remove forced throw error.
