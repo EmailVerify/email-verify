035ab020a1ac14a50a19f6987ec86c5b9a7c4ce1
sancowinx <jm-sarn@hotmail.com>
2016-01-09 13:55:21 +0700

    fix using wrong variables

--- methods/readfromfile.js
+++ methods/readfromfile.js
@@ -4,6 +4,6 @@ var _ = require('lodash');
 module.exports.getAddressFromTextFile = function(filepath) {
   var fileContent = fs.readFileSync(filepath, 'utf-8');
   var addressList = fileContent.split("\n");
-  var addressFiltered = _.without(addressList,'');
+  var addressesFiltered = _.without(addressList,'');
   return addressesFiltered;
 }
