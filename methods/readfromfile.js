40841219f818b5e9a7e53c5e8dd0a81dbd3b623f
sancowinx <jm-sarn@hotmail.com>
2016-01-09 14:18:17 +0700

    added checking if file is exist

--- methods/readfromfile.js
+++ methods/readfromfile.js
@@ -2,8 +2,25 @@ var fs = require('fs');
 var _ = require('lodash');
 
 module.exports.getAddressFromTextFile = function(filepath) {
-  var fileContent = fs.readFileSync(filepath, 'utf-8');
-  var addressList = fileContent.split("\n");
-  var addressesFiltered = _.without(addressList,'');
-  return addressesFiltered;
+  var isExist = true;
+  try {
+    var fileContent = fs.readFileSync(filepath, 'utf-8');
+  } catch (e){
+    if (e.code === 'ENOENT') console.log('File not found!');
+    else console.log('Another error: ', e);
+    isExist = false;
+  }
+  finally {
+
+    // if file is not found, do not run this!
+    if (!isExist) {
+      throw new Error("SOMETHING BROKE")
+    } else {
+      var addressList = fileContent.split("\n");
+      var addressesFiltered = _.without(addressList,'');
+      return addressesFiltered;
+    };
+  }
 }
+
+// TODO: Clean up, if posible
